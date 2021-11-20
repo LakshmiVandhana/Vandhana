6df3c8e0070c373c05fb3edf6c41e533c8d0067a
LakshmiVandhana <lakshmivandhanag@gmail.com>
2021-11-20 19:16:49 +0530

    this is my first prgm in java

--- forloop.sh
+++ forloop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#timestable
+#timestable - print out a multiplication table
 for i in 1 2 3
 do
  for j in 1 2 3
  do
    value='expr $i\*$j'
-   echo -n"$value"
+   echo "$value"
   done
   echo
  done
+
 
